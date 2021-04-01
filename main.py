8d877c359528a00571d56eeefb1f7fc36f20302d
guidoenr <guidoenr4@gmail.com>
2021-04-01 17:10:33 -0300

    threading + playsound

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-from playsound import playsound
+from time import sleep
+
+from pydub import AudioSegment
+from pydub.playback import play
+import threading
 
 
 if __name__ == '__main__':
-    playsound('/resources/responses/greetings/bye-boss.mp3')
+    mp3File = 'resources/responses/success/right-away-boss.mp3'
+    sound = AudioSegment.from_mp3(mp3File)
+    thread = threading.Thread(target=play, args=(sound,))
+    thread.start()
+    print(1)
+    sleep(2)
+    print(2)
+    sleep(3)
