3e8c354b520865d8e36ab4287b90b343ad74e2ba
guidoenr <guidoenr4@gmail.com>
2021-07-04 13:15:06 -0300

    geometry' && git push && exit

--- startup.sh
+++ startup.sh
@@ -4,10 +4,13 @@ sleep 1
 
 mpg123 ~/Desktop/jarvis/resources/jarvisintro.mp3 & 
 
-sleep 3
+sleep 1
 
-firefox github.com/guidoenr &
-chromium google.com.ar &
+chromium github.com/guidoenr &
+sleep 2
+
+gnome-terminal -e htop --geometry 87x27+2050+200 &
+gnome-terminal --geometry 87x27+3000+200 &
 
 exit
 
